2a2c1abb9cf9289e412ecc4b6cb2830cf58c43fc
Elson <huangyuhao@huangyuhaodeMacBook-Pro.local>
2017-03-02 15:23:17 +0800

    網路練習-利用data方式下載網路圖片-利用非同步下載的方式gcd

--- helloDownloadImage/ViewController.swift
+++ helloDownloadImage/ViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+         if let url = URL(string:"https://cdn.codementor.io/assets/topic/category_header/swift-7b180a2fc122c5bd24fcd6520d279452.png"){
+            DispatchQueue.global().async {
+                do {
+                    let myData = try Data(contentsOf: url)
+                    let image = UIImage(data: myData)
+                    
+                    DispatchQueue.main.async {
+                        self.myImage.image = image
+                    }
+               
+                } catch  {
+                    print(error.localizedDescription)
+                }
+            }
+           
+            
+        
+        }
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
